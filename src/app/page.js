d312fe6ab0d138d48a69977b1c92801f99a08f55
Hiroto Kanda HOME <hirotokanda@akane.waseda.jp>
2025-07-21 12:06:13 +0900

    2025/07/21 HOME

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import {
     RssIcon,
     Bars3Icon, // Hamburger menu icon
     XMarkIcon, // Close icon
+  EnvelopeIcon,
 } from '@heroicons/react/24/outline';
 
 // Sidebar navigation links
@@ -19,6 +20,7 @@ const navigation = [
   { name: 'Home', href: 'https://home-d0ot0s2er-jainasus-projects.vercel.app/', icon: HomeIcon },
   { name: 'Github', href: 'https://github.com/JaiNasu?tab=repositories', icon:  CommandLineIcon},
   { name: 'Moodle', href:'https://wsdmoodle.waseda.jp/my/courses.php', icon: AcademicCapIcon },
+  { name: 'Gmail', href: 'https://mail.google.com/mail/u/0/#inbox', icon: EnvelopeIcon },
 ];
 
 export default function HomePage() {
