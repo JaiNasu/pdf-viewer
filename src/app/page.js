fad13d3dd700c67462ab6f17d10f92123308d0dc
Hiroto Kanda HOME <hirotokanda@akane.waseda.jp>
2025-07-21 12:15:36 +0900

    2025/07/21 HOME

--- src/app/page.js
+++ src/app/page.js
@@ -79,7 +79,7 @@ export default function HomePage() {
 
         <div className="flex items-center justify-center p-10">
           <main className="w-full max-w-2xl text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               Home
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
